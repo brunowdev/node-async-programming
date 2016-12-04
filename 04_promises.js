23e90d0df7cd532d14fc4305de2d93677b19b5b0
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2016-12-04 21:55:22 -0200

    Promises

---
+++ 04_promises.js
@@ -0,0 +1,15 @@
+﻿function _myPromiseCreator() {
+   
+    const myPromise = new Promise(function (resolve, reject) {
+        reject('error')
+    }); 
+
+    return myPromise;
+}
+
+_myPromiseCreator().then(function(resolvedValue) {
+    console.log('resolvedValue ', resolvedValue);
+})
+.catch(function(rejectedValue) {
+    console.log('rejectedValue ', rejectedValue);
+})
